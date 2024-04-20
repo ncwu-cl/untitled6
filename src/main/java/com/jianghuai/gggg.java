1ea3ce12447d52ac94583f87e1bb457d956741b9
chenyi <chenyi@loverxtt.com>
2024-04-20 15:46:49 +0800

    第一次提交

---
+++ src/main/java/com/jianghuai/gggg.java
@@ -0,0 +1,8 @@
+package com.jianghuai;
+
+public class gggg {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+    }
+}
