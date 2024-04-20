9ccc3dff92ab03a3b8c21a775fcbc537dd632315
chenyi <chenyi@loverxtt.com>
2024-04-20 16:44:22 +0800

    ymm forever add

--- src/main/java/com/jianghuai/gggg.java
+++ src/main/java/com/jianghuai/gggg.java
@@ -2,7 +2,7 @@ package com.jianghuai;
 
 public class gggg {
     public static void main(String[] args) {
-        System.out.println("杨苗苗");
+        System.out.println("myLover -> 杨苗苗");
 
     }
 }
