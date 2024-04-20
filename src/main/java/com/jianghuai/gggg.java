58b4e8a66877093a69ddc28931eafb6eb59b5394
星星 <83969050+ncwu-cl@users.noreply.github.com>
2024-04-20 21:59:57 +0800

    Update gggg.java

--- src/main/java/com/jianghuai/gggg.java
+++ src/main/java/com/jianghuai/gggg.java
@@ -3,11 +3,10 @@ package com.jianghuai;
 public class gggg {
     public static void main(String[] args) {
         System.out.println("my_favorite_lover is 杨苗苗");
-        System.out.println("my_favorite_lover is 杨苗苗");
-        System.out.println("my_favorite_lover is 杨苗苗");
-        System.out.println("my_favorite_lover is 杨苗苗");
-        System.out.println("my_favorite_lover is 杨苗苗");
+       
         System.out.println("杨苗苗");
+
+        
 
         
 
